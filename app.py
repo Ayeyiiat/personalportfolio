443e03328d6a3f14a176c1c9c6ad09d8d77a28c0
Isaac Turkson <iturkson@u.rochester.edu>
2021-07-15 16:52:29 +0000

    Built skeleton for personal website

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
+from flask_behind_proxy import FlaskBehindProxy
+
+
+app = Flask(__name__)
+proxied = FlaskBehindProxy(app)
+app.config['SECRET_KEY'] = '51ac8ed6210948128125f8ae2a4ad67f'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
